daedfbfe4b3649e4586783a6b4e8097271ec5822
Mohsen Araghi <araghi13@outlook.com>
2023-05-22 15:49:32 +0330

    fk added for BrdCategory ,shpCheckout , shpOrder , shpPreparing

--- MainBakeryShop/BakeryDB/dbo/Tables/BrdCategory.sql
+++ MainBakeryShop/BakeryDB/dbo/Tables/BrdCategory.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[BrdCategory]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
     [Name] NVARCHAR(50) NOT NULL, 
     [Thumbnaile] NVARCHAR(50) NOT NULL, 
     [BkCTId] BIGINT NOT NULL
+    CONSTRAINT [FK_BrdCategory_BkCTId_InfBakeryBrdCategory] FOREIGN KEY ([BkCTId]) REFERENCES [InfBakeryBrdCategory]([Id])
 )
--- MainBakeryShop/BakeryDB/dbo/Tables/shpPreparing.sql
+++ MainBakeryShop/BakeryDB/dbo/Tables/shpPreparing.sql
@@ -6,5 +6,7 @@
     [IsFinished] BIT NOT NULL DEFAULT 0, 
     [DateTime] DATETIME2 NOT NULL,
     [UserId] BIGINT NOT NULL, 
-    [UserBakeryId] BIGINT NOT NULL
+    [UserBakeryId] BIGINT NOT NULL, 
+    CONSTRAINT [FK_Id_shpPreparing_UserId_usrCostumer] FOREIGN KEY ([UserId]) REFERENCES [usrCostumer]([Id]), 
+    CONSTRAINT [FK_shpPreparing_UserBakeryId_usrCostumer] FOREIGN KEY ([UserBakeryId]) REFERENCES[usrCostumer]([Id])
 )
