4c3d40efba9a9b0ed659f7421d692674b821c1aa
Mohsen Araghi <araghi13@outlook.com>
2023-05-24 15:07:42 +0330

    Db infs change on infctbrd brd catg tables

--- MainBakeryShop/BakeryDB/dbo/Tables/BrdCategory.sql
+++ MainBakeryShop/BakeryDB/dbo/Tables/BrdCategory.sql
@@ -2,7 +2,5 @@
 (
 	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
     [Name] NVARCHAR(50) NOT NULL, 
-    [Thumbnaile] NVARCHAR(50) NOT NULL, 
-    [BkCTId] BIGINT NOT NULL
-    CONSTRAINT [FK_BrdCategory_BkCTId_InfBakeryBrdCategory] FOREIGN KEY ([BkCTId]) REFERENCES [InfBakeryBrdCategory]([Id])
+    [Thumbnaile] NVARCHAR(50) NOT NULL 
 )
--- MainBakeryShop/BakeryDB/dbo/Tables/InfBakeryBrdCategory.sql
+++ MainBakeryShop/BakeryDB/dbo/Tables/InfBakeryBrdCategory.sql
@@ -3,6 +3,8 @@
 	[Id] BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
     [CategoryId] BIGINT NOT NULL, 
     [BreadId] BIGINT NOT NULL, 
+    [BakeryId] BIGINT NOT NULL, 
     CONSTRAINT [FK_InfBakeryBrdCategory_BreadId_Bread] FOREIGN KEY ([BreadId]) REFERENCES [Bread]([Id]), 
-    CONSTRAINT [FK_InfBakeryBrdCategory_CategoryId_brdCategory] FOREIGN KEY ([CategoryId]) REFERENCES [brdCategory]([Id])
+    CONSTRAINT [FK_InfBakeryBrdCategory_CategoryId_brdCategory] FOREIGN KEY ([CategoryId]) REFERENCES [brdCategory]([Id]), 
+    CONSTRAINT [FK_InfBakeryBrdCategory_BakeryId_InfBakery] FOREIGN KEY ([BakeryId]) REFERENCES [InfBakery]([Id])
 )
