877e3b4df3c35f9738bfaf4f8c5bd140b968bae8
Mohsen Araghi <araghi13@outlook.com>
2023-05-31 14:33:54 +0330

    isNullOrEmptyfunc Added And spSignup Modified

---
+++ MainBakeryShop/BakeryDB/dbo/Functions/IsNullOrEmpty.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION [dbo].[IsNullOrEmpty](@String NVARCHAR(50)) 
+RETURNS bit
+As
+Begin
+    
+	Declare @res bit;
+
+	if(ISNULL(LTRIM(RTRIM(@String)),'')='')
+		begin
+			set @res = 1;
+		end
+	else
+		begin
+			set @res = 0;
+		end
+
+	Return @res;
+End
--- MainBakeryShop/BakeryDB/dbo/Stored Procedures/spSignUp.sql
+++ MainBakeryShop/BakeryDB/dbo/Stored Procedures/spSignUp.sql
@@ -1,4 +1,5 @@
 ﻿CREATE PROCEDURE [dbo].[SignUp]
+    @UserRole nvarchar(50),
     @FirstName NVARCHAR(50) , 
     @MiddleName NVARCHAR(50) , 
     @LastName NVARCHAR(50) , 
@@ -9,10 +10,108 @@
     @Password NVARCHAR(50) , 
     @Email NVARCHAR(100) 
 AS
---begin
-----      (select * from usrCostumer where FirstName=@FirstName and MiddleName = @MiddleName and LastName=@LastName and Address=@Address and City=@City and Phone=@Phone and  UserName=@UserName and  Password=@Password and Email=@Email)
-----SELECT @param1, @param2
-----scope_
---end
+begin
+	DECLARE @IsUserRoleNull bit;
+	DECLARE @IsFirstNameNull bit;
+    DECLARE @IsLastNameNull bit; 
+    DECLARE @IsAddressNull bit; 
+    DECLARE @IsCityNull bit;
+    DECLARE @IsPhoneNull bit; 
+    DECLARE @IsUserNameNull bit; 
+    DECLARE @IsPasswordNull bit; 
+    DECLARE @IsEmailNull bit;
+
+	SELECT  @IsUserRoleNull = [dbo].[IsNullOrEmpty](@UserRole);
+	SELECT  @IsFirstNameNull = [dbo].[IsNullOrEmpty](@FirstName);
+	SELECT  @IsLastNameNull = [dbo].[IsNullOrEmpty](@LastName);
+	SELECT  @IsAddressNull = [dbo].[IsNullOrEmpty](@Address);
+	SELECT  @IsCityNull = [dbo].[IsNullOrEmpty](@City);
+	SELECT  @IsPhoneNull = [dbo].[IsNullOrEmpty](@Phone);
+	SELECT  @IsUserNameNull = [dbo].[IsNullOrEmpty](@UserName);
+	SELECT  @IsPasswordNull = [dbo].[IsNullOrEmpty](@Password);
+	SELECT  @IsEmailNull = [dbo].[IsNullOrEmpty](@Email);
+
+	if(@IsUserRoleNull = 1)
+		begin
+			select 'UnkonwnUserRole' as Id;
+			return 0;
+		end
+	else
+	begin
+		if(@UserRole = N'Baker')
+			begin
+				if(@IsFirstNameNull = 0 and @IsLastNameNull = 0 and @IsAddressNull = 0
+				 and @IsPhoneNull = 0 and @IsUserNameNull = 0
+				 and @IsPasswordNull = 0 and @IsEmailNull = 0    )
+					begin
+						 INSERT INTO [dbo].[usrBaker]
+									([FirstName]
+									,[MiddleName]
+									,[LastName]
+									,[Address]
+									,[Phone]
+									,[UserName]
+									,[Password]
+									,[Email])
+							  VALUES
+									(@FirstName
+									,@MiddleName
+									,@LastName
+									,@Address
+									,@Phone
+									,@UserName
+									,@Password
+									,@Email)
+						select SCOPE_IDENTITY() as Id;
+						return 1;	
+					end
+				else
+					begin
+						select 'SomeNullValuesExists' as Id;
+						return 0;
+					end
+			end
+			else if(@UserRole = N'User')
+			begin
+				if(@IsFirstNameNull = 0 and @IsLastNameNull = 0 and @IsAddressNull = 0
+				 and @IsCityNull = 0 and @IsPhoneNull = 0 and @IsUserNameNull = 0
+				 and @IsPasswordNull = 0 and @IsEmailNull = 0    )
+					begin
+						 INSERT INTO [dbo].[usrCostumer]
+									([FirstName]
+									,[MiddleName]
+									,[LastName]
+									,[Address]
+									,[City]
+									,[Phone]
+									,[UserName]
+									,[Password]
+									,[Email])
+							  VALUES
+									(@FirstName
+									,@MiddleName
+									,@LastName
+									,@Address
+									,@Phone
+									,@City
+									,@UserName
+									,@Password
+									,@Email)
+						select SCOPE_IDENTITY() as Id;
+						return 2;
+					end
+				else
+					begin
+						select 'SomeNullValuesExists' as Id;
+						return 0;
+					end
+			end
+			else
+			begin
+				select 'InvalidUserType' Id;
+				return 0;
+			end
+	end
+end
 	
 RETURN 0
