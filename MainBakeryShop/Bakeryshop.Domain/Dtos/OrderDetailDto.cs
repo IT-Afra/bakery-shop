e07c7d84ea5c7f06fe6d514de6cb8d8fe4f3834c
Mohsen Araghi <araghi13@outlook.com>
2023-06-11 18:16:24 +0330

    Add OrderDetailDto And OrderDto

---
+++ MainBakeryShop/Bakeryshop.Domain/Dtos/OrderDetailDto.cs
@@ -0,0 +1,34 @@
+﻿using Bakeryshop.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakeryshop.Domain.Dtos
+{
+    public class OrderDetailDto
+    {
+        [Display(Name = "شناسه")]
+        public long Id { get; set; }
+
+        [Display(Name = "شناسه سفارش")]
+        public long OrderId { get; set; }
+
+        [Display(Name = "شناسه نوع نان")]
+        public long TypeBreadId { get; set; }
+
+        [Display(Name = "قیمت")]
+        [Required(ErrorMessage = ErrorReource.Required)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "تعداد")]
+        [Required(ErrorMessage = ErrorReource.Required)]
+        [MaxLength(4, ErrorMessage = ErrorReource.MaxLength)]
+        public int Amount { get; set; }
+
+        [Display(Name = "تعداد سفارش")]
+        public int DeliveryAmount { get; set; }
+    }
+}
---
+++ MainBakeryShop/Bakeryshop.Domain/Dtos/OrderDto.cs
@@ -0,0 +1,44 @@
+﻿using Bakeryshop.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakeryshop.Domain.Dtos
+{
+    public class OrderDto
+    {
+        [Display(Name = "شناسه")]
+        public long Id { get; set; }
+
+        [Display(Name = "شناسه کاربر")]
+        public long UserId { get; set; }
+
+        [Display(Name = "شناسه زمانبندی")]
+        public long SchaduleId { get; set; }
+
+        [Display(Name = "تاریخ ثبت به حروف")]
+        [Required(ErrorMessage = ErrorReource.Required)]
+        [MaxLength(21, ErrorMessage = ErrorReource.MaxLength)]
+        public string RegisterDateTime { get; set; } = null!;
+
+        [Display(Name = "تاریخ ثبت به عدد")]
+        [Required(ErrorMessage = ErrorReource.Required)]
+        [MaxLength(8, ErrorMessage = ErrorReource.MaxLength)]
+        public long RegisterDateTimeL { get; set; }
+
+        [Display(Name = "تاریخ ارسال به حروف")]
+        public string? DeliveryDateTime { get; set; }
+
+
+        [Display(Name = "تاریخ ارسال به عدد")]
+        public long? DeliveryDateTimeL { get; set; }
+
+        [Display(Name = "قیمت کل")]
+        [Required(ErrorMessage = ErrorReource.Required)]
+        public decimal TotalPrice { get; set; }
+
+    }
+}
