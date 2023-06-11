9e0b740b2c499509bc24f7b96a5c3e16ba970bba
MohammadPirzadeh <mohammadreza.guardy@gmail.com>
2023-06-11 14:18:22 +0330

    Add Connetcion String

--- MainBakeryShop/Bakeryshop.Infrastructure/EntityFramework/BakershopContext.cs
+++ MainBakeryShop/Bakeryshop.Infrastructure/EntityFramework/BakershopContext.cs
@@ -4,8 +4,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Bakeryshop.Infrastructure.EntityFramework
 {
+    
+    
     public class BakershopContext : DbContext
     {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer("Data Source=.;Initial Catalog=Bakeryshop;Integrated Security=True");
+        }
         public virtual DbSet<bksTypeBread> bksTypeBreads { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
