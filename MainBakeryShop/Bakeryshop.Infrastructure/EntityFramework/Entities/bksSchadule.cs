a107f0078600843029bb51f21a6a205f0e7be401
MohammadPirzadeh <mohammadreza.guardy@gmail.com>
2023-06-11 15:31:41 +0330

    mpEntities

---
+++ MainBakeryShop/Bakeryshop.Infrastructure/EntityFramework/Entities/bksSchadule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakeryshop.Infrastructure.EntityFramework.Entities
+{
+    public class bksSchadule
+    {
+        public long Id { get; set; }
+        public string OrderDate { get; set; } = null!;
+        public long OrderDateL { get; set; }
+        public string OrderTime { get; set; } = null!;
+        public int OrderTimel { get; set; }
+        public long? ModifierUser { get; set; }
+        public long? ModifierDate { get; set; }
+    }
+}
