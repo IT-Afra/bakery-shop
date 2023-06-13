e7caec8ec75ab2b33dcc6e3fd8e76a9bc66c5bd3
Vahid Pezeshki <vahid-pezeshki@outlook.com>
2023-06-13 15:21:38 +0330

    Add bksOrderDetail Modified Navigation

--- MainBakeryShop/Bakeryshop.Infrastructure/EntityFramework/Entities/bksOrderDetail.cs
+++ MainBakeryShop/Bakeryshop.Infrastructure/EntityFramework/Entities/bksOrderDetail.cs
@@ -17,7 +17,7 @@ namespace Bakeryshop.Infrastructure.EntityFramework.Entities
         public long? ModifierUser { get; set; }
         public long? ModifierDate { get; set; }
 
-        public virtual bksOrder BksOrders { get; set; } = null!;
-        public virtual bksTypeBread BksTypeBreads { get; set; } = null!;
+        public virtual bksOrder BksOrder { get; set; } = null!;
+        public virtual bksTypeBread BksTypeBread { get; set; } = null!;
     }
 }
