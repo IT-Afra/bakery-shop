7d23848483ae0f11b6be980baf31788f3e5f4c69
Vahid Pezeshki <vahid-pezeshki@outlook.com>
2023-06-18 17:49:30 +0330

    Add nullability to OrderDetailDto

--- MainBakeryShop/Bakeryshop.Domain/Dtos/SchaduleDetailDto.cs
+++ MainBakeryShop/Bakeryshop.Domain/Dtos/SchaduleDetailDto.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System;
-
-using Bakeryshop.Framework;
+﻿using Bakeryshop.Framework;
 using System.ComponentModel.DataAnnotations;
 
 namespace Bakeryshop.Domain.Dtos
@@ -9,27 +6,27 @@ namespace Bakeryshop.Domain.Dtos
     public class SchaduleDetailDto
     {
         [Display(Name = "شناسه")]
-        public long Id { get; set; }
+        public long? Id { get; set; }
 
         [Display(Name = "شناسه زمانبندی")]
         [Required(ErrorMessage = ErrorReource.Required)]
-        public long SchaduleId { get; set; }
+        public long? SchaduleId { get; set; }
 
         [Display(Name = "شناسه نوع نان")]
         [Required(ErrorMessage = ErrorReource.Required)]
-        public long TypeBreadId { get; set; }
+        public long? TypeBreadId { get; set; }
 
         [Display(Name = "قیمت")]
         [Required(ErrorMessage = ErrorReource.Required)]
-        public decimal Price { get; set; }
+        public decimal? Price { get; set; }
 
         [Display(Name = "تعداد")]
         [Required(ErrorMessage = ErrorReource.Required)]
-        public int Amount { get; set; }
+        public int? Amount { get; set; }
 
         [Display(Name = "وضعیت")]
         [Required(ErrorMessage = ErrorReource.Required)]
-        public bool IsActive { get; set; }
+        public bool? IsActive { get; set; }
 
 
     }
