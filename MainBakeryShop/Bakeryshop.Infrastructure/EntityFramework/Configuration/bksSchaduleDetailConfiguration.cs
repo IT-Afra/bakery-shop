0134ba37558cbf841035d42891d647f762e884a9
Vahid Pezeshki <vahid-pezeshki@outlook.com>
2023-06-19 13:39:46 +0330

    Edit ToTable method in bksSchaduleDetail.

--- MainBakeryShop/Bakeryshop.Infrastructure/EntityFramework/Configuration/bksSchaduleDetailConfiguration.cs
+++ MainBakeryShop/Bakeryshop.Infrastructure/EntityFramework/Configuration/bksSchaduleDetailConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using Bakeryshop.Infrastructure.EntityFramework.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bakeryshop.Infrastructure.EntityFramework.Configuration
 {
@@ -13,14 +8,10 @@ namespace Bakeryshop.Infrastructure.EntityFramework.Configuration
     {
         public void Configure(EntityTypeBuilder<bksSchaduleDetail> builder)
         {
-            builder.ToTable("bksSchaduleDetail");
-
+            builder.ToTable(nameof(bksSchaduleDetail));
             builder.HasKey(b => b.Id);
-
             builder.Property(s => s.Price).HasMaxLength(18).IsRequired();
-
             builder.Property(s => s.Amount).IsRequired();
-
             builder.Property(s => s.IsActive).IsRequired();
 
             builder.HasOne(s => s.Schadule)
