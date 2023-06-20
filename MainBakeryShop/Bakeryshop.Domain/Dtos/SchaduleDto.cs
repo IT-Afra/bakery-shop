75a0b1cac551fd3ac01d10026d145a7a5af22db5
Mohsen Araghi <araghi13@outlook.com>
2023-06-20 18:03:43 +0330

    SchaduleProfile added & SchaduleDto modifed

--- MainBakeryShop/Bakeryshop.Domain/Dtos/SchaduleDto.cs
+++ MainBakeryShop/Bakeryshop.Domain/Dtos/SchaduleDto.cs
@@ -6,29 +6,27 @@ namespace Bakeryshop.Domain.Dtos
 {
     public class SchaduleDto
     {
+        public SchaduleDto()
+        {
+            SchaduleDetails = new List<SchaduleDetailDto>();
+        }
+
         [Display(Name ="شناسه")]
         public long Id { get; set; }
 
         [Display(Name = " تاریخ سفارش به حروف")]
         [Required(ErrorMessage = ErrorReource.Required)]
         [MaxLength(10, ErrorMessage = ErrorReource.MaxLength)]
-        public string OrderDate { get; set; } = null!;
-
-
-        [Display(Name = " تاریخ سفارش به عدد")]
-        [Required(ErrorMessage = ErrorReource.Required)]
-        public long OrderDateL { get; set; }
+        public string SchaduleDate { get; set; } = null!;
 
 
         [Display(Name = "ساعت سفارش به حروف")]
         [MaxLength(10, ErrorMessage = ErrorReource.MaxLength)]
         [Required(ErrorMessage = ErrorReource.Required)]
-        public string OrderTime { get; set; } = null!;
+        public string SchaduleTime { get; set; } = null!;
 
+        List<SchaduleDetailDto> SchaduleDetails { get; set; }
 
-        [Display(Name = "ساعت سفارش به عدد")]
-        [Required(ErrorMessage = ErrorReource.Required)]
-        public int OrderTimeL { get; set; }
 
     }
 }
