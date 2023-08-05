1d9bdb055b193b5aa3353a47a29d451f874d5f63
AFRA Network <itafranet@gmail.com>
2023-08-05 08:12:57 -0700

    LayoutAdded
    Layout
    
    Co-Authored-By: Mohsen Araghi <87324761+Mohsenarg@users.noreply.github.com>
    Co-Authored-By: Hamed Nikbakht <123782996+HamedNikbakht@users.noreply.github.com>
    Co-Authored-By: Vahid Pezeshki <78328217+vahid-pezeshki@users.noreply.github.com>
    Co-Authored-By: Mohammad Pirzadeh <85078384+mohammadpirzadeh@users.noreply.github.com>

--- MainBakeryShop/bakeryadmin-react/src/pages/MainLayout.tsx
+++ MainBakeryShop/bakeryadmin-react/src/pages/MainLayout.tsx
@@ -1,10 +1,60 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 type Props = {}
 
 const MainLayout = (props: Props) => {
+  const
+    {
+      token: { colorBgContainer },
+    } = theme.useToken();
   return (
-    <div>MainLayout</div>
+    <>
+      <Layout style={{ height: "100vh" }}>
+        <Header style={{ display: 'flex', alignItems: 'center' }}>
+          <div style={{color:"white", textDecoration:"underline"}}>
+            <h2>پنل ادمین فروشگاه نان</h2>
+          </div>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+          />
+        </Header>
+        <Layout>
+          <Sider
+            breakpoint="lg"
+            collapsedWidth="0"
+          >
+            <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={['4']}
+              items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+                (icon, index) => ({
+                  key: String(index + 1),
+                  icon: React.createElement(icon),
+                  label: `nav ${index + 1}`,
+                }),
+              )}
+            />
+          </Sider>
+          <Layout>
+            <Content style={{ margin: '24px 16px 0' }}>
+              <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
+                <Outlet />
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>ABA Design ©2023 Created by ABA's BOSS</Footer>
+          </Layout>
+        </Layout>
+      </Layout>
+
+    </>
   )
 }
 
