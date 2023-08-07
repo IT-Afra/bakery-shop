bf14732c2733b45616ade25f0d714b045bbaa890
Mohsen Araghi <araghi13@outlook.com>
2023-08-07 12:21:48 +0330

    Add Header Items
    Add Header Items
    
    Co-Authored-By: Hamed Nikbakht <123782996+HamedNikbakht@users.noreply.github.com>
    Co-Authored-By: Mohammad Pirzadeh <85078384+mohammadpirzadeh@users.noreply.github.com>

--- MainBakeryShop/bakeryadmin-react/src/App.tsx
+++ MainBakeryShop/bakeryadmin-react/src/App.tsx
@@ -6,21 +6,40 @@ import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/login/LoginPage';
 import AuthenticateLayout from './pages/AuthenticateLayout';
 import MainLayout from './pages/MainLayout';
+import { ConfigProvider, theme } from 'antd';
+import faIR from 'antd/locale/fa_IR';
+const { defaultAlgorithm, darkAlgorithm } = theme;
+
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+
+  const setThemeLayout = (isDark:boolean):void => {
+    setIsDarkMode(isDark);
+  }
+
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="login" element={<AuthenticateLayout />} >
-          <Route path="*" element={<LoginPage />} />
-        </Route>
-        <Route path="*" element={<MainLayout />} >
-          <Route path="home" element={<HomePage />} />
-          <Route path="typebread" element={<TypeBreadPage />} />
-          <Route path='*' element={<Navigate to="/home" replace />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <ConfigProvider locale={faIR} direction='rtl' componentSize='middle' theme={{
+      algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+      token: {
+        colorPrimary: '#8b008b',
+        fontFamily: 'Vazirmatn',
+
+      },
+    }} >
+      <HashRouter>
+        <Routes>
+          <Route path="login" element={<AuthenticateLayout />} >
+            <Route path="*" element={<LoginPage />} />
+          </Route>
+          <Route path="*" element={<MainLayout setThemeLayout={setThemeLayout} />} >
+            <Route path="home" element={<HomePage />} />
+            <Route path="typebread" element={<TypeBreadPage />} />
+            <Route path='*' element={<Navigate to="/home" replace />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </ConfigProvider>
   );
 }
 
--- MainBakeryShop/bakeryadmin-react/src/pages/MainLayout.tsx
+++ MainBakeryShop/bakeryadmin-react/src/pages/MainLayout.tsx
@@ -1,27 +1,89 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined, DownOutlined, LogoutOutlined } from '@ant-design/icons';
+import { Col, Layout, Menu, Row, Space, Switch, theme, Image, Dropdown, Button, MenuProps } from 'antd';
+import "./../assets/images/Logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const { Header, Content, Footer, Sider } = Layout;
 
-type Props = {}
+type Props = {
+  setThemeLayout(isDark: boolean): void
+}
 
-const MainLayout = (props: Props) => {
+const MainLayout = ({ setThemeLayout }: Props) => {
   const
     {
       token: { colorBgContainer },
     } = theme.useToken();
+
+  const items: MenuProps['items'] = [
+    {
+      label: 'پروفایل',
+      key: '1',
+      icon: <UserOutlined />,
+    },
+    {
+      label: 'خروج از حساب کاربری',
+      key: '2',
+      icon: <LogoutOutlined />,
+    }
+  ];
+
+  const handleMenuClick: MenuProps['onClick'] = (e) => {
+    console.log('click', e);
+  };
+
+  const menuProps = {
+    items,
+    onClick: handleMenuClick,
+  };
+
+
   return (
     <>
       <Layout style={{ height: "100vh" }}>
-        <Header style={{ display: 'flex', alignItems: 'center' }}>
-          <div style={{color:"white", textDecoration:"underline"}}>
-            <h2>پنل ادمین فروشگاه نان</h2>
-          </div>
+        <Header style={{ backgroundColor: "white" }}>
+          <Row>
+            <Col span={12} >
+              <Row>
+                <Space style={{ lineHeight: "1.25" }}>
+                  <Space direction="horizontal" align='center' >
+                    <h2>پنل ادمین فروشگاه نان</h2>
+                  </Space>
+                  <Space direction="horizontal" align='center' >
+                    <Image preview={false} src='Logo.png' alt='logo' width={30} height={30} />
+                  </Space>
+                </Space>
+              </Row>
+            </Col>
+            <Col span={12}>
+              <Row style={{ float: "left" }} >
+                <Space direction="horizontal">
+                  <Space direction="horizontal" align='center' style={{paddingLeft:'10px'}} >
+                    <Switch
+                      checkedChildren={<FontAwesomeIcon icon={"sun"} style={{ color: "yellow" }} />}
+                      unCheckedChildren={<FontAwesomeIcon icon={"moon"} style={{ color: "darkblue" }} />}
+                      defaultChecked
+                    />
+                  </Space>
+                </Space>
+                <Space direction="horizontal" align='center' >
+                  <Dropdown menu={menuProps} >
+                    <Button>
+                      <Space>
+                        ادمین
+                        <DownOutlined />
+                      </Space>
+                    </Button>
+                  </Dropdown>
+                </Space>
+              </Row>
+            </Col>
+          </Row>
           <Menu
-            theme="dark"
+            // theme='light'
             mode="horizontal"
           />
         </Header>
@@ -29,9 +91,10 @@ const MainLayout = (props: Props) => {
           <Sider
             breakpoint="lg"
             collapsedWidth="0"
+            color='white'
           >
             <Menu
-              theme="dark"
+              // theme='light'
               mode="inline"
               defaultSelectedKeys={['4']}
               items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
