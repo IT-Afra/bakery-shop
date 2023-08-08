73348cfbd37e260ed060a97cc11f7e9f3329d4b4
Mohsen Araghi <araghi13@outlook.com>
2023-08-08 18:37:47 +0330

    AddEdit&DeleteOp
    Co-Authored-By: Mohammad Pirzadeh <85078384+mohammadpirzadeh@users.noreply.github.com>

--- MainBakeryShop/bakeryadmin-react/src/pages/typeBread/TypeBreadPage.tsx
+++ MainBakeryShop/bakeryadmin-react/src/pages/typeBread/TypeBreadPage.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Spin, Row, Col, Table } from 'antd'
+import { Spin, Row, Col, Table, Grid, Button } from 'antd'
 import { ITypeBread } from '../../models/ITypeBread'
 import type { ColumnsType } from 'antd/es/table';
+import { EditFilled } from "@ant-design/icons";
 import axios from 'axios'
+const { useBreakpoint } = Grid;
 
 type Props = {}
 
 const TypeBreadPage = (props: Props) => {
   const [isSpinning, setIsSpinning] = React.useState(true)
   const [typeBreads, setTypeBreads] = React.useState<ITypeBread[]>([])
+  const screens = useBreakpoint();
+  const [selectedRowKeys, setSelectedRowKeys] = React.useState<React.Key[]>([]);
+  const [selectedRowID, setSelectedRowID] = React.useState<React.Key[]>([0]);
 
   const getTypeBreads = () => {
     axios.get<ITypeBread[]>('https://localhost:7239/api/typeBread')
@@ -26,9 +31,79 @@ const TypeBreadPage = (props: Props) => {
 
   React.useEffect(() => {
     getTypeBreads();
-  }, []);
+  }, [typeBreads]);
+
+
+  const save = (typeBread: ITypeBread) => {
+    axios.post<ITypeBread>("https://localhost:7239/api/typeBread", {
+      id: typeBread.id,
+      name: typeBread.name,
+      isActive: typeBread.isActive,
+      photoGuid: typeBread.photoGuid
+    })
+      .then((response) => {
+        setIsSpinning(true);
+        let data: ITypeBread[] = [response.data, ...typeBreads];
+        setTypeBreads(data);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+      .finally(() => {
+        setIsSpinning(false)
+      });
+  }
+
+  const onSelectChange = (newSelectedRowKeys: React.Key[], selectedRow: ITypeBread[]) => {
+    let ids = selectedRow.map((x) => {
+      return (x.id);
+    });
+    setSelectedRowID(ids)
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  const columnsMobile: ColumnsType<ITypeBread> = [
+    {
+      title: "لیست نان های موجود",
+      dataIndex: "Bread",
+      render: (record: ITypeBread) => (
+        <div>
+          <span>
+            <div style={{ width: "25%" }}>ردیف : </div>
+            <div style={{ width: "75%", fontWeight: "bold" }}>{record.id}</div >
+          </span>
+          <span>
+            <div style={{ width: "25%" }}>نام : </div>
+            <div style={{ width: "75%", fontWeight: "bold" }}>{record.name}</div >
+          </span>
+          <span>
+            <div style={{ width: "25%" }}>وضعیت : </div>
+            <div style={{ width: "75%", fontWeight: "bold" }}>{record.isActive}</div >
+          </span>
+          <span>
+            <div style={{ width: "25%" }}>عکس : </div>
+            <div style={{ width: "75%", fontWeight: "bold" }}>{record.photoGuid}</div >
+          </span>
+          <span>
+            <div style={{ width: "25%" }}>ویرایش : </div>
+            <Button icon={<EditFilled />} onClick={() => save(record)}></Button>
+            <div style={{ width: "75%", fontWeight: "bold" }}>
+            </div >
+          </span>
+        </div>
+      )
+    }]
 
   const columns: ColumnsType<ITypeBread> = [
+    {
+      title: 'ردیف',
+      dataIndex: 'id',
+    },
     {
       title: 'عنوان',
       dataIndex: 'name',
@@ -38,6 +113,15 @@ const TypeBreadPage = (props: Props) => {
       dataIndex: 'isActive',
       render: (isActive: boolean) => isActive ? "فعال" : "غیرفعال"
     },
+    {
+      title: 'عکس',
+      dataIndex: 'photoGuid',
+    },
+    {
+      title: 'ویرایش',
+      dataIndex: 'id',
+      render: (record: ITypeBread) => <Button icon={<EditFilled />} onClick={() => save(record)}></Button>
+    }
   ]
 
   return (
@@ -47,7 +131,7 @@ const TypeBreadPage = (props: Props) => {
           انواع نان
         </Col>
       </Row>
-      <Table columns={columns} dataSource={typeBreads} />
+      <Table rowSelection={rowSelection} columns={screens.xs ? columnsMobile : columns} dataSource={typeBreads} />
     </Spin>
   )
 }
